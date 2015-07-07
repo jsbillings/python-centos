be258ca048fd116383174fa3d444be92d4ab6661
Brian Stinson <brian@bstinson.com>
2015-07-06 20:56:28 -0500

    better default handling for the Certificate class

--- centos/centos_cert.py
+++ centos/centos_cert.py
@@ -4,7 +4,10 @@ from OpenSSL import crypto
 class CentOSCert(object):
     DEFAULT_USER_FILE="~/.centos.cert"
 
-    def __init__(self, filename=DEFAULT_USER_FILE):
+    def __init__(self, filename=None):
+        if filename is None:
+            filename = self.DEFAULT_USER_FILE
+
         with open(os.path.expanduser(filename),'r') as certfile:
             self._cert = crypto.load_certificate(crypto.FILETYPE_PEM, certfile.read())
             self.__dict__.update(dict(self._cert.get_subject().get_components()))
