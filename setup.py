f0793741905ad4e5f7ae6fa4120aaa9c51184f49
Brian Stinson <brian@bstinson.com>
2015-07-27 08:05:17 -0500

    use my email address for submission to pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="centos",
     description="Client-side modules for CentOS Services",
     author="Brian Stinson",
-    author_email="centos-devel@centos.org",
+    author_email="brian@bstinson.com",
     packages = find_packages(),
     license = 'MIT License',
     version = '0.0.1',
