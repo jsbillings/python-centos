765384769107d3762256e1b520d30b1939437d03
Brian Stinson <brian@bstinson.com>
2015-09-10 08:28:41 -0500

    add the AccountSystem to centos

--- centos/__init__.py
+++ centos/__init__.py
@@ -1,5 +1,6 @@
 import defaults
 
 from centos_cert import CentOSUserCert
+from fedora.client import AccountSystem
 
-__all__ = ('CentOSUserCert','defaults')
+__all__ = ('CentOSUserCert','defaults', 'AccountSystem')
