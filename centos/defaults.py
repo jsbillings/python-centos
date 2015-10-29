e73eb9c2f2e7f7d859290557b7f83c56f237945f
Brian Stinson <brian@bstinson.com>
2015-10-28 21:26:17 -0500

    switch to the prod version of FAS

--- centos/defaults.py
+++ centos/defaults.py
@@ -1,5 +1,5 @@
 USER_CERT_FILE = '~/.centos.cert'
 SERVER_CA_CERT_FILE = '~/.centos-server-ca.cert'
 UPLOAD_CA_CERT_FILE = '~/.centos-upload-ca.cert'
-FAS_TOPURL = 'https://accounts.dev.centos.org'
-FAS_CRL = 'https://accounts.dev.centos.org/ca/crl.pem'
+FAS_TOPURL = 'https://accounts.centos.org'
+FAS_CRL = 'https://accounts.centos.org/ca/crl.pem'
